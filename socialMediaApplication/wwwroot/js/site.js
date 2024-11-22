0a80c3c6882729f64e3b52c25cc3aa06b2491c4f
alomda.alslmat@gmail.com <alomda.alslmat@gmail.com>
2024-11-22 23:58:28 +0400

    new updates

--- socialMediaApplication/wwwroot/js/site.js
+++ socialMediaApplication/wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿var swiper = new Swiper(".multiple-slide-carousel", {
+            loop: true,
+        slidesPerView: 6,
+        spaceBetween: 20,
+        autoplay: true,
+        navigation: {
+            nextEl: ".multiple-slide-carousel .swiper-button-next",
+        prevEl: ".multiple-slide-carousel .swiper-button-prev",
+   },
+        breakpoints: {
+            1920: {
+            slidesPerView: 9,
+        spaceBetween: 20
+    },
+        1028: {
+            slidesPerView: 7,
+        spaceBetween: 20
+    },
+        990: {
+            slidesPerView: 3,
+        spaceBetween: 20
+    }
+  }
+ });
