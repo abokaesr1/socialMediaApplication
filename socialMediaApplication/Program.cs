c0a7b7e9d6cb06b2cf5fcd8c8b7a85b9d01c9898
alomda.alslmat@gmail.com <alomda.alslmat@gmail.com>
2024-11-23 11:01:13 +0400

    migration

--- socialMediaApplication/Program.cs
+++ socialMediaApplication/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using socialMediaApplication.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// inject the sql db from model 
+
+
+builder.Services.AddDbContext<ApplicationContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
